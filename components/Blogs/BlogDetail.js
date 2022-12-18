fde2f6046b5a08b0c1ac5aee6fb3cb041d4d6daa
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2022-12-18 23:09:16 +0530

    sdfgh

--- components/Blogs/BlogDetail.js
+++ components/Blogs/BlogDetail.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ORG_NAME } from '../../lib/constants';
 import FormattedDate from '../FormattedDate';
-import markdownStyles from '../markdown-styles.module.css';
 import SharingIcons from '../SharingIcons';
 
 export default function BlogDetail({ title, coverImage, date, content }) {
@@ -35,15 +34,15 @@ export default function BlogDetail({ title, coverImage, date, content }) {
           </div>
         </div>
         <h2 className='text-2xl px-2 text-center mt-5 font-bold'>{title}</h2>
-        <div className="mb-7 px-2 mt-1 text-sm italic text-center lowercase">
+        <div className="mb-7 px-2 mt-1 text-sm italic  lowercase">
           Posted on <FormattedDate dateString={date} />
         </div>
         <div className="mb-8 sm:mx-0 w-full mx-auto relative">
           <Image src={coverImage} alt={title} height={9} width={16}
             objectFit='cover' layout='responsive' objectPosition='center' />
         </div>
-        <div className="w-3/4 text-justify mx-auto mb-8">
-          <div  class="prose prose-slate" className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: content }}></div>
+        <div className="w-3/4 mx-auto mb-8">
+          <div  class="prose prose-slate max-w-none" dangerouslySetInnerHTML={{ __html: content }}></div>
         </div>
         <div className='my-5 text-center'>
           <div className='mt-4'>
